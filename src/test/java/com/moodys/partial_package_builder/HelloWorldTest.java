197e9fefadb92dfe6990862e6449a97d885d338b
Zheng Li <loylizh@gmail.com>
2013-09-17 14:45:11 +0800

    Add Hello World test case

---
+++ src/test/java/com/moodys/partial_package_builder/HelloWorldTest.java
@@ -0,0 +1,13 @@
+package com.moodys.partial_package_builder;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+	
+	@Test
+	public void testHelloWorld(){
+		assertTrue(true);
+	}
+}
